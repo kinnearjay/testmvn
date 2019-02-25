5be7a8a00b24f674134bb1f23a5e4770e96bd1a1
Jordan <Ecrofiar@gmail.com>
2019-02-25 16:53:04 +0000

    fixed error with the if statement by making a variable for user input

--- src/test/java/miscResults.java
+++ src/test/java/miscResults.java
@@ -5,12 +5,12 @@ public class miscResults {
     Blackjack bj = new Blackjack();
     @Test
     public void invalidScore( ){
-        Assert.assertEquals("-1",bj.play(32,44));
-        Assert.assertEquals("-1",bj.play(55,33));
+        Assert.assertEquals("the score was not invalid","-1",bj.play(32,44));
+        Assert.assertEquals("the score was not invalid","-1",bj.play(55,33));
 
     }
     @Test public void bothBust(){
-        Assert.assertEquals("0",bj.play(22,22));
+        Assert.assertEquals("both players were not bust?","0",bj.play(22,22));
     }
 
 }
